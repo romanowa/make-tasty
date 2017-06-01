966b3ad227104685ab422f702653ef567eca20b4
a.ramanava <alina.romanowa@gmail.com>
2017-06-01 13:37:58 +0300

    components structure created

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+/* Created by a.ramanava on 1/6/2017. */
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+
+}
